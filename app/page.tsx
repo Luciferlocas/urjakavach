f63e11db527b485ba89fd5f6af3281b0de4a1173
Luciferlocas <rohitsomvanshi2003@gmail.com>
2024-09-07 20:17:03 +0530

    changed the redirect path

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid place-content-center items-center w-screen min-h-screen">
-      <Link className="bg-blue-500 px-6 py-2 rounded-lg shadow-md" href="http://localhost:3000/u/">Login</Link>
+      <Link className="bg-blue-500 px-6 py-2 rounded-lg shadow-md" href="/u">Login</Link>
     </div>
   );
 }
